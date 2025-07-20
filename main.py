e9bb8c2de1e726c429f7bc118016004c6fec4e2f
ioprog <ioprog@proton.me>
2025-07-20 20:38:44 +0400

    add function for UI rendering📳

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
+from ui import render_ui
 
+render_ui({'0':"",'1':""})
---
+++ ui.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def render_ui(functions:dict)->None:
+    st.title("MaoMao")
+    function = st.selectbox("Выберите тип входных данных",functions.keys())
+    input_str = st.text_input("Введите входные данные")
+    if st.button('Старт'):
+        try:
+            fu = functions[function]
+            st.code(fu(input_str))
+        except Exception as e:
+            st.error(f"Произошла ошибка:\n{e}")     
